e649713cab519a5c5c0ae560eebd257c5f7fceac
umut <abrenji55@gmail.com>
2024-06-24 10:34:40 +0300

    added some content to index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
+import { Container, Box } from "@chakra-ui/react";
+
 const Page = () => {
-  return <div>Home Page</div>;
+  return (
+    <Container>
+      <Box borderRadius="lg" bg="gray" p={3} align="center">
+        Hello I am umut mete a software engineer from turkey
+      </Box>
+    </Container>
+  );
 };
 
 export default Page;
