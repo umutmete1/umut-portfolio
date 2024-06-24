0b45647e94f663a7025fbeebd413ea25d14834b5
umut <abrenji55@gmail.com>
2024-06-24 15:38:18 +0300

    added darkmode

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,18 @@
-import { Container, Box } from "@chakra-ui/react";
+import { Container, Box, Heading } from "@chakra-ui/react";
 
 const Page = () => {
   return (
     <Container>
-      <Box borderRadius="lg" bg="gray" p={3} align="center">
-        Hello I am umut mete a software engineer from turkey
+      <Box borderRadius="lg" bg="red" p={3} align="center">
+        Hello I am a software engineer based in turkey!
+      </Box>
+
+      <Box display={{ md: "flex" }}>
+        <Box flexGrow={1}>
+          <Heading as="h2" variant="page-title">
+            Umut Mete
+          </Heading>
+        </Box>
       </Box>
     </Container>
   );
