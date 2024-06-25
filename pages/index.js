ea4127cb520ed999cae6a26fb058b26efc20970f
umut <abrenji55@gmail.com>
2024-06-25 17:31:43 +0300

    added section to index page

--- pages/index.js
+++ pages/index.js
@@ -1,38 +1,26 @@
-import { Container, Box, Image, Heading } from "@chakra-ui/react";
+import {
+  Container,
+  Box,
+  Image,
+  Heading,
+  UnorderedList,
+  ListItem,
+} from "@chakra-ui/react";
 import { motion } from "framer-motion";
+import Section from "@/components/section";
 
 const MotionImage = motion(Image);
 
 const Page = () => {
   return (
     <Container>
-      <Box display="flex" alignItems="center" justifyContent="center">
-        <MotionImage
-          boxSize="400px"
-          src="/images/umut.jpg"
-          alt="umut mete"
-          objectFit="contain"
-          initial={{ y: "100vh" }}
-          animate={{ y: 0 }}
-          transition={{ type: "spring", stiffness: 50, damping: 20 }}
-        />
-      </Box>
-      <Box
-        alignItems="center"
-        justifyContent="center"
-        m={10}
-        borderTop="3px solid white"
-      >
+      <Box display="flex" alignItems="center" justifyContent="center"></Box>
+      <Section delay={0.1}>
         <Heading as="h1" variant="page-title">
           Umut Mete
         </Heading>
         <p>Software Engineer</p>
-        <Heading
-          as="h2"
-          variant="page-title"
-          mt={8}
-          borderBottom="3px solid white"
-        >
+        <Heading as="h2" variant="page-title" mt={8}>
           About
         </Heading>
         <p>
@@ -45,15 +33,31 @@ const Page = () => {
           time working out or socializing, I am a very extraverted person I like
           meeting new people and spending time with the people I love.
         </p>
-        <Heading
-          as="h2"
-          variant="page-title"
-          mt={8}
-          borderBottom="3px solid white"
-        >
+        <Heading as="h2" variant="page-title" mt={8}>
           Skills
         </Heading>
-      </Box>
+        <Heading as="h3" variant="section-title">
+          Back-End
+        </Heading>
+        <UnorderedList>
+          <ListItem>Microservices</ListItem>
+          <ListItem>Rest APIs</ListItem>
+          <ListItem>.NET 6+</ListItem>
+          <ListItem>SignalR</ListItem>
+          <ListItem>Entity framework / LINQ</ListItem>
+          <ListItem>.NET Identity</ListItem>
+        </UnorderedList>
+        <Heading as="h3" variant="section-title">
+          Front-End
+        </Heading>
+        <UnorderedList>
+          <ListItem>HTML</ListItem>
+          <ListItem>CSS</ListItem>
+          <ListItem>Javascript</ListItem>
+          <ListItem>React</ListItem>
+          <ListItem>Next.js</ListItem>
+        </UnorderedList>
+      </Section>
     </Container>
   );
 };
